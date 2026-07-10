9d73a9196ee56d0601452fc6d1c3adfa3d16695a
Fatima Notron <fatimanotron@gmail.com>
2026-07-11 02:26:02 +0330

    ch2: solve exercise 5

---
+++ 2.C-FUNDAMENTALS/Exercises/5.c
@@ -0,0 +1,17 @@
+/*
+ * the Question:
+ * Which of the following are not legal C identifiers?
+ * (a) 100_bottles
+ * (b) _100_bottles
+ * (c) one__hundred__bottles
+ * (d) bottles_by_the_hundred_
+ */
+
+/*
+ * Answer:
+ * (a) is not a legal identifier because it starts with a digit.
+ *
+ * (b), (c), and (d) are all legal identifiers.
+ * An identifier may begin with an underscore, contain multiple
+ * underscores, or end with an underscore.
+ */
